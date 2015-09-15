087993777b227b7e15b2d44f7537b500b3b012c5
Tim van der Graaff <timvdg45@gmail.com>
2015-09-15 10:55:51 -0500

    Update database-setup.php

--- wp-content/plugins/landing-page-redirector/library/database-setup.php
+++ wp-content/plugins/landing-page-redirector/library/database-setup.php
@@ -4,7 +4,7 @@
 		//The database server is called here.
 		protected function get_database_server() {
 			
-			$database_server = @mysqli_connect("localhost", "payneer_wp630", "-31D@x1tSP")
+			$database_server = @mysqli_connect("server", "username", "password")
 			Or die("<h1>The website had a hiccup.  Try reloading this page.</h1>");
 			
 			return $database_server;
